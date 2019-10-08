e378247be8571d1d5550f174feea9b5407e82370
Kamil Polanski <polanski88@gmail.com>
2019-10-08 17:39:50 +0200

    Add basic files

---
+++ src/comments.js
@@ -0,0 +1,31 @@
+import {
+  ADD_COMMENT,
+  REMOVE_COMMENT,
+  THUMB_UP_COMMENT
+} from './actions';
+
+export function comments(state = [], action) {
+  switch (action.type) {
+    case ADD_COMMENT:
+      return [{
+        id: action.id,
+        text: action.text,
+        votes: 0
+      },
+      ...state];
+    case REMOVE_COMMENT:
+      return [{
+        comments: state.filter(comment => comment.id !== action.id)
+      }];
+    case THUMB_UP_COMMENT:
+      return state.map(comment => {
+        if (comment.id === action.id) {
+          return { ...comment, votes: comment.votes + 1 }
+        }
+        return comment;
+      });
+    default:
+      return state;
+  }
+}
+
--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { reducer } from './reducer';
+import {
+	addComment
+} from './actions.js';
+const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+store.dispatch(addComment('pierwszy komentarz'));
+store.dispatch(addComment('drugi komentarz'));
+console.log(store);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
